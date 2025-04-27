38c4cc0db77c02157a332bdc7088f6fef46e9611
GVR developer <43445955+TDRGVR@users.noreply.github.com>
2025-04-27 16:52:05 -0400

    Update Localization.lua

--- Locale/Localization.lua
+++ Locale/Localization.lua
@@ -3,7 +3,6 @@ ArchiTotemLocale = setmetatable({}, {
     __index = function(t, k)
         local v = tostring(k)
         rawset(t, k, v)
-		DEFAULT_CHAT_FRAME:AddMessage(string.format("|cFF33FF99[Debug]|r Solicitud de traducción: %q", v))
 		
         if (LOCALE ~= "enUS") and (LOCALE ~= "enGB") then
             DEFAULT_CHAT_FRAME:AddMessage(string.format("[ArchiTotem] Texto no traducido: %q (Idioma: %s)", v, LOCALE))
